61ba80c54172a869ea7483c854365b6dc66383df
DarioRuizGon <darioruiz334@gmail.com>
2025-07-30 21:30:00 +0200

    modelo pipeline comentado

--- app_model.py
+++ app_model.py
@@ -23,59 +23,59 @@ def hello():
     <p>Usa /api/v1/predict con los parámetros acidity, chlorides, so, sulphates</p>
     """
 
-# @app.route('/api/v1/predict', methods = ['GET'])
-# def predict():
-#     model = joblib.load("my_model.joblib")
-
-        
-#     acidity = request.args.get('acidity', None)
-#     chlorides = request.args.get('chlorides', None)
-#     so = request.args.get('so', None)
-#     sulphates = request.args.get('sulphates', None)
-
-    
-
-#     if acidity is None or chlorides is None or so is None or sulphates is None:
-#         return 'Args empty, not enough data to predict'
-#     else:
-#         prediction = model.predict([[float(acidity),float(chlorides),float(so), float(sulphates)]])
-
-
-    
-#     return jsonify({'prediction': int(prediction[0])})
-
-
 @app.route('/api/v1/predict', methods = ['GET'])
 def predict():
+    model = joblib.load("my_model.joblib")
 
-    with open('wine_model.pkl', 'rb') as f:
-        model = pickle.load(f)
-
+        
     acidity = request.args.get('acidity', None)
     chlorides = request.args.get('chlorides', None)
     so = request.args.get('so', None)
     sulphates = request.args.get('sulphates', None)
 
-    def prediction(X):
-        pred = int(model.predict(X)[0])
-        mapping = {0: "red", 1: "white"}
-        return jsonify({'prediction': mapping[pred]})
-
-    X = [acidity, chlorides, so, sulphates]
-
-    def type_processing(X):
-        result = []
-        for n in X:
-            try:
-                value = float(n)
-            except:
-                value = None
-            result.append(value)
-        return result
     
-    X = np.array(type_processing(X)).reshape(-1, 4)
 
-    return prediction(X)
+    if acidity is None or chlorides is None or so is None or sulphates is None:
+        return 'Args empty, not enough data to predict'
+    else:
+        prediction = model.predict([[float(acidity),float(chlorides),float(so), float(sulphates)]])
+
+
+    
+    return jsonify({'prediction': int(prediction[0])})
+
+
+# @app.route('/api/v1/predict', methods = ['GET'])
+# def predict():
+
+#     with open('wine_model.pkl', 'rb') as f:
+#         model = pickle.load(f)
+
+#     acidity = request.args.get('acidity', None)
+#     chlorides = request.args.get('chlorides', None)
+#     so = request.args.get('so', None)
+#     sulphates = request.args.get('sulphates', None)
+
+#     def prediction(X):
+#         pred = int(model.predict(X)[0])
+#         mapping = {0: "red", 1: "white"}
+#         return jsonify({'prediction': mapping[pred]})
+
+#     X = [acidity, chlorides, so, sulphates]
+
+#     def type_processing(X):
+#         result = []
+#         for n in X:
+#             try:
+#                 value = float(n)
+#             except:
+#                 value = None
+#             result.append(value)
+#         return result
+    
+#     X = np.array(type_processing(X)).reshape(-1, 4)
+
+#     return prediction(X)
 
 if __name__ == '__main__':
     app.run(debug=True)
